307c5f263344ca0394df82682a5b42207804d9e8
Kavi Arthur <nammount19@gmail.com>
2025-08-23 18:03:19 +0700

    thêm phụ trợ và lương cơ sở

---
+++ PMS/Controllers/PhuTroController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PMS.Controllers
+{
+    public class PhuTroController : Controller
+    {
+        // View chính
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Action AJAX render lại bảng (trả về partial view khác nhau)
+        public IActionResult RenderPhuTro(string loai, int thang, int nam)
+        {
+            switch (loai)
+            {
+                case "DienThoai":
+                    return PartialView("_PhuTroDienThoai");  // Views/PhuTro/_PhuTroDienThoai.cshtml
+
+                case "CDPhi":
+                    return PartialView("_PhuTroCDPhi");      // Views/PhuTro/_PhuTroCDPhi.cshtml
+
+                case "TNCN":
+                    return PartialView("_PhuTroTNCN");       // Views/PhuTro/_PhuTroTNCN.cshtml
+
+                default:
+                    return Content("Loại phụ trợ không hợp lệ.");
+            }
+        }
+    }
+}
--- PMS/obj/Debug/net5.0/PMS.RazorTargetAssemblyInfo.cs
+++ PMS/obj/Debug/net5.0/PMS.RazorTargetAssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyProductAttribute("PMS")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ca0fc2eb9b22c1ed377fa7cdb805d5d32f6780b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acac7f13aa70b3dc0fb18496081a45b8f5d7c7d6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PMS.Views")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
